f9a52a7719e36ede1cdd11e318a57d3b24f8f5a8
kriahja <kriahja@gmail.com>
2015-11-26 12:25:39 +0100

    Fixed folder mixup and working with GenericService

---
+++ LifeÍnEsbjergClient/LifeInEsbjergGateway/GenericService/WebApiService.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeInEsbjergGateway.Services
+{
+   public class WebApiService
+    {
+        private WebApiService(string baseUri)
+        {
+            BaseUri = baseUri;
+        }
+
+        private static WebApiService _instance;
+
+        public static WebApiService Instance
+        {
+            get { return _instance ?? (_instance = new WebApiService(ConfigurationManager.AppSettings["WebApiUri"])); }
+        }
+
+        public string BaseUri { get; private set; }
+
+        public async Task<T> GetAsync<T>(string action)
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync(action);
+
+                string json = await result.Content.ReadAsStringAsync();
+                if (result.IsSuccessStatusCode)
+                {
+                    return JsonConvert.DeserializeObject<T>(json);
+                }
+
+                throw new ApiException(result.StatusCode, json);
+            }
+            //using (var client = new HttpClient())
+            //{
+            //    HttpResponseMessage response =
+            //        client.GetAsync("http://localhost:17348/api/company/").Result;
+            //    return response.Content.ReadAsAsync<IEnumerable<Company>>().Result;
+            //}
+        }
+        private string BuildActionUri(string action)
+        {
+            return BaseUri + action;
+        }
+    }
+}
